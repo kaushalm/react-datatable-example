437a2485902cedcf2544781e422b9735e41232c7
kaushalm <kaushalmehra1987@gmail.com>
2021-02-03 18:46:40 +0530

    serach filter changes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,42 @@ require("isomorphic-fetch");
 export default function App() {
   const [data, setData] = useState([]);
   const [q, setQ] = useState("");
+  const [searchterm, setSearchTerm] = useState("");
+
+  window.originaldata = [];
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
       .then(json => {
         setData(json);
+        //console.log(json);
+        window.originaldata = [...originaldata, ...json];
       });
   }, []);
 
+  function search(event) {
+    //console.log(window.originaldata);
+    //console.log(event.target.value);
+    let filtered = data.filter(d => {
+      let serchabledata = d.userId + d.id + d.title + d.body;
+      if (serchabledata.includes(event.target.value)) {
+        return d;
+      }
+    });
+
+    console.log(filtered);
+    if (filtered.length > 0) {
+      setData(filtered);
+    } else {
+      console.log(window.originaldata);
+      setData(window.originaldata);
+    }
+  }
+
   return (
     <div>
-      <input type="text" />
+      <input type="text" onChange={search} />
       <Datatable data={data} />
     </div>
   );
--- src/Datatable.js
+++ src/Datatable.js
@@ -2,14 +2,21 @@ import React from "react";
 
 export default function Datatable({ data }) {
   const columns = data[0] && Object.keys(data[0]);
+
+  function sort(e) {
+    console.log(e.target.innerHTML);
+  }
+
   return (
     <table>
       <thead>
-        <tr>{data[0] && columns.map(heading => <th>{heading}</th>)}</tr>
+        <tr onClick={sort}>
+          {data[0] && columns.map(heading => <th>{heading}</th>)}
+        </tr>
       </thead>
       <tbody>
         {data.map(row => (
-          <tr>
+          <tr key={row["id"]}>
             {columns.map(column => (
               <td>{row[column]}</td>
             ))}
